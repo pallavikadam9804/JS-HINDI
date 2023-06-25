6c4da476f48cb4aedd1e45444c7a6467e0b8a045
pallavikadam9804 <135444441+pallavikadam9804@users.noreply.github.com>
2023-06-25 14:06:23 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"  //not a pure number//
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" --> 33 //
+// "33abc" --> NaN (Not a Number)//
+// true --> 1
+// false --> 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 --> true; 0 --> false
+// "" --> false
+// "pallavi" --> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
