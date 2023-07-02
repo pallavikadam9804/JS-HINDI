ba97bc99f1fe94dcc9bd3ee45ca72130e61ceda6
pallavikadam9804 <135444441+pallavikadam9804@users.noreply.github.com>
2023-07-02 13:43:14 +0000

    Comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+    //Comparision//
+
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  // null has been converted into 0//
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);  //These are not same because there datatypes are different//
